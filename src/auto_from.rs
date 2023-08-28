87c9d79fc24bbe4960ab9357b50e7b792fbd340a
Ákos Vandra-Meyer <axos88@gmail.com>
2023-08-28 08:18:02 +0200

    fix partial move issue when using a custom_fn and moving converters

--- src/auto_from.rs
+++ src/auto_from.rs
@@ -177,6 +177,7 @@ impl DeriveIntoContext {
         self.fields
             .clone()
             .into_iter()
+            .sorted_by_key(|fd| fd.get_by_name(FieldClass::From(struct_name.clone())).unwrap_or(fd.default_opts.clone()).custom_fn.is_empty())
             .map(|fd| {
                 let Fd {
                     name,
